54a23fb690c222b0bf7fbfa1020bd7d8af68c992
Vitor Aguiar <cnhaguiar@gmail.com>
2022-03-16 17:24:40 -0300

    um sonho

---
+++ script.js
@@ -0,0 +1,19 @@
+/**/
+
+'use scrict'
+
+let contador = 1
+document.getElementById('radio1').checked = true
+
+setInterval( function(){
+    passarImagens(); 
+}, 3000)
+
+const passarImagens = () =>{
+    contador++
+    if(contador > 4)
+        contador == 1
+    
+    document.getElementById('radio'+contador).checked = true
+    
+}
